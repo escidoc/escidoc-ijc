8f4ef83afe3cff2ae857610de1d0f6b99d629249
SWA <SWA@5c398c58-3d0a-0410-a38c-d0825f6816e0>
2010-01-25 09:26:52 +0000

    code style
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/branches/eSciDocCoreClient/1.1/eSciDocCoreClient/@33050 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- src/java/de/escidoc/core/client/rest/serviceLocator/ContextRestServiceLocator.java
+++ src/java/de/escidoc/core/client/rest/serviceLocator/ContextRestServiceLocator.java
@@ -43,215 +43,210 @@ import de.escidoc.core.om.ContextHandler;
  * @author SWA
  * 
  */
-public class ContextRestServiceLocator extends RestServiceMethod implements
-		ContextHandler {
+public class ContextRestServiceLocator extends RestServiceMethod
+    implements ContextHandler {
 
-	private static final String PATH_CONTEXT = "/ir/context";
+    private static final String PATH_CONTEXT = "/ir/context";
 
-	public String close(final String contextId, final String taskParam)
-			throws RemoteException, OptimisticLockingException,
-			SystemException, MissingMethodParameterException, LockingException,
-			InvalidStatusException, AuthenticationException,
-			StreamNotFoundException, AuthorizationException,
-			ContextNotFoundException, InvalidXmlException {
+    public String close(final String contextId, final String taskParam)
+        throws RemoteException, OptimisticLockingException, SystemException,
+        MissingMethodParameterException, LockingException,
+        InvalidStatusException, AuthenticationException,
+        StreamNotFoundException, AuthorizationException,
+        ContextNotFoundException, InvalidXmlException {
 
-		return post(PATH_CONTEXT + "/" + contextId + "/close", taskParam);
-	}
+        return post(PATH_CONTEXT + "/" + contextId + "/close", taskParam);
+    }
 
-	public String open(final String contextId, final String taskParam)
-			throws java.rmi.RemoteException, OptimisticLockingException,
-			SystemException, MissingMethodParameterException, LockingException,
-			InvalidStatusException, AuthenticationException,
-			StreamNotFoundException, AuthorizationException,
-			ContextNotFoundException, InvalidXmlException {
+    public String open(final String contextId, final String taskParam)
+        throws java.rmi.RemoteException, OptimisticLockingException,
+        SystemException, MissingMethodParameterException, LockingException,
+        InvalidStatusException, AuthenticationException,
+        StreamNotFoundException, AuthorizationException,
+        ContextNotFoundException, InvalidXmlException {
 
-		return post(PATH_CONTEXT + "/" + contextId + "/open", taskParam);
-	}
+        return post(PATH_CONTEXT + "/" + contextId + "/open", taskParam);
+    }
 
-	public String retrieveMembers(final String contextId, final String filter)
-			throws RemoteException, SystemException,
-			MissingMethodParameterException, AuthenticationException,
-			AuthorizationException, ContextNotFoundException,
-			InvalidXmlException {
+    public String retrieveMembers(final String contextId, final String filter)
+        throws RemoteException, SystemException,
+        MissingMethodParameterException, AuthenticationException,
+        AuthorizationException, ContextNotFoundException, InvalidXmlException {
 
-		return post(PATH_CONTEXT + "/" + contextId
-				+ "/resources/members/filter", filter);
-	}
+        return post(PATH_CONTEXT + "/" + contextId
+            + "/resources/members/filter", filter);
+    }
 
-	public String retrieveMembers(final String contextId, final HashMap filter)
-			throws RemoteException, SystemException,
-			MissingMethodParameterException, AuthenticationException,
-			AuthorizationException, ContextNotFoundException,
-			InvalidXmlException {
+    public String retrieveMembers(final String contextId, final HashMap filter)
+        throws RemoteException, SystemException,
+        MissingMethodParameterException, AuthenticationException,
+        AuthorizationException, ContextNotFoundException, InvalidXmlException {
 
-		return get(PATH_CONTEXT + "/" + contextId + "/resources/members",
-				filter);
-	}
+        return get(PATH_CONTEXT + "/" + contextId + "/resources/members",
+            filter);
+    }
 
-	public String retrieveAdminDescriptor(final String contextId,
-			final String admId) throws RemoteException, SystemException,
-			AdminDescriptorNotFoundException, MissingMethodParameterException,
-			AuthenticationException, AuthorizationException,
-			ContextNotFoundException {
+    public String retrieveAdminDescriptor(
+        final String contextId, final String admId) throws RemoteException,
+        SystemException, AdminDescriptorNotFoundException,
+        MissingMethodParameterException, AuthenticationException,
+        AuthorizationException, ContextNotFoundException {
 
-		return get(PATH_CONTEXT + "/" + contextId
-				+ "/admin-descriptors/admin-descriptor/" + admId);
-	}
+        return get(PATH_CONTEXT + "/" + contextId
+            + "/admin-descriptors/admin-descriptor/" + admId);
+    }
 
-	public String retrieveAdminDescriptors(final String contextId)
-			throws RemoteException, SystemException,
-			MissingMethodParameterException, AuthenticationException,
-			AuthorizationException, ContextNotFoundException {
+    public String retrieveAdminDescriptors(final String contextId)
+        throws RemoteException, SystemException,
+        MissingMethodParameterException, AuthenticationException,
+        AuthorizationException, ContextNotFoundException {
 
-		return get(PATH_CONTEXT + "/" + contextId + "/admin-descriptors");
-	}
+        return get(PATH_CONTEXT + "/" + contextId + "/admin-descriptors");
+    }
 
-	public String updateAdminDescriptor(final String in0, final String in1)
-			throws RemoteException, OptimisticLockingException,
-			SystemException, AdminDescriptorNotFoundException,
-			MissingMethodParameterException, AuthenticationException,
-			AuthorizationException, ContextNotFoundException,
-			InvalidXmlException {
+    public String updateAdminDescriptor(final String in0, final String in1)
+        throws RemoteException, OptimisticLockingException, SystemException,
+        AdminDescriptorNotFoundException, MissingMethodParameterException,
+        AuthenticationException, AuthorizationException,
+        ContextNotFoundException, InvalidXmlException {
 
-		throw new SystemException(500, "Method not yet supported", "");
-		// return put(PATH_CONTEXT + "/" + contextId
-		// + "/admin-descriptors/admin-descriptor/" + admId);
-	}
+        throw new SystemException(500, "Method not yet supported", "");
+        // return put(PATH_CONTEXT + "/" + contextId
+        // + "/admin-descriptors/admin-descriptor/" + admId);
+    }
 
-	/**
-	 * See Interface for functional description.
-	 * 
-	 * @param contextId
-	 * @throws RemoteException
-	 * @throws SystemException
-	 * @throws LockingException
-	 * @throws MissingMethodParameterException
-	 * @throws InvalidStatusException
-	 * @throws AuthenticationException
-	 * @throws ContextNotFoundException
-	 * @throws AlreadyPublishedException
-	 * @throws AuthorizationException
-	 * @see de.escidoc.core.om.ContextHandler#delete(String)
-	 */
-	public void delete(final String contextId) throws RemoteException,
-			SystemException, LockingException, MissingMethodParameterException,
-			InvalidStatusException, AuthenticationException,
-			ContextNotFoundException, AlreadyPublishedException,
-			AuthorizationException {
+    /**
+     * See Interface for functional description.
+     * 
+     * @param contextId
+     * @throws RemoteException
+     * @throws SystemException
+     * @throws LockingException
+     * @throws MissingMethodParameterException
+     * @throws InvalidStatusException
+     * @throws AuthenticationException
+     * @throws ContextNotFoundException
+     * @throws AlreadyPublishedException
+     * @throws AuthorizationException
+     * @see de.escidoc.core.om.ContextHandler#delete(String)
+     */
+    public void delete(final String contextId) throws RemoteException,
+        SystemException, LockingException, MissingMethodParameterException,
+        InvalidStatusException, AuthenticationException,
+        ContextNotFoundException, AlreadyPublishedException,
+        AuthorizationException {
 
-		del(PATH_CONTEXT + "/" + contextId);
-	}
+        del(PATH_CONTEXT + "/" + contextId);
+    }
 
-	/**
-	 * See Interface for functional description.
-	 * 
-	 * @param contextXml
-	 * @return
-	 * @throws RemoteException
-	 * @throws SystemException
-	 * @throws MissingAttributeValueException
-	 * @throws MissingContentException
-	 * @throws MissingMdRecordException
-	 * @throws ReferencedResourceNotFoundException
-	 * @throws AuthenticationException
-	 * @throws AuthorizationException
-	 * @throws ContextNotFoundException
-	 * @throws InvalidContentException
-	 * @throws RelationPredicateNotFoundException
-	 * @throws ReadonlyAttributeViolationException
-	 * @throws FileNotFoundException
-	 * @throws MissingMethodParameterException
-	 * @throws InvalidStatusException
-	 * @throws ReadonlyElementViolationException
-	 * @throws ContentModelNotFoundException
-	 * @throws InvalidXmlException
-	 * @throws MissingElementValueException
-	 * @see de.escidoc.core.om.ContextHandler#create(String)
-	 */
-	public String create(final String contextXml) throws RemoteException,
-			SystemException, MissingAttributeValueException,
-			MissingContentException, MissingMdRecordException,
-			ReferencedResourceNotFoundException, AuthenticationException,
-			AuthorizationException, ContextNotFoundException,
-			InvalidContentException, RelationPredicateNotFoundException,
-			ReadonlyAttributeViolationException, FileNotFoundException,
-			MissingMethodParameterException, InvalidStatusException,
-			ReadonlyElementViolationException, ContentModelNotFoundException,
-			InvalidXmlException, MissingElementValueException {
+    /**
+     * See Interface for functional description.
+     * 
+     * @param contextXml
+     * @return
+     * @throws RemoteException
+     * @throws SystemException
+     * @throws MissingAttributeValueException
+     * @throws MissingContentException
+     * @throws MissingMdRecordException
+     * @throws ReferencedResourceNotFoundException
+     * @throws AuthenticationException
+     * @throws AuthorizationException
+     * @throws ContextNotFoundException
+     * @throws InvalidContentException
+     * @throws RelationPredicateNotFoundException
+     * @throws ReadonlyAttributeViolationException
+     * @throws FileNotFoundException
+     * @throws MissingMethodParameterException
+     * @throws InvalidStatusException
+     * @throws ReadonlyElementViolationException
+     * @throws ContentModelNotFoundException
+     * @throws InvalidXmlException
+     * @throws MissingElementValueException
+     * @see de.escidoc.core.om.ContextHandler#create(String)
+     */
+    public String create(final String contextXml) throws RemoteException,
+        SystemException, MissingAttributeValueException,
+        MissingContentException, MissingMdRecordException,
+        ReferencedResourceNotFoundException, AuthenticationException,
+        AuthorizationException, ContextNotFoundException,
+        InvalidContentException, RelationPredicateNotFoundException,
+        ReadonlyAttributeViolationException, FileNotFoundException,
+        MissingMethodParameterException, InvalidStatusException,
+        ReadonlyElementViolationException, ContentModelNotFoundException,
+        InvalidXmlException, MissingElementValueException {
 
-		return put(PATH_CONTEXT, contextXml);
-	}
+        return put(PATH_CONTEXT, contextXml);
+    }
 
-	/**
-	 * See Interface for functional description.
-	 * 
-	 * @param contextId
-	 * @param contextXml
-	 * @return
-	 * @throws RemoteException
-	 * @throws SystemException
-	 * @throws MissingLicenceException
-	 * @throws ReadonlyVersionException
-	 * @throws LockingException
-	 * @throws ComponentNotFoundException
-	 * @throws MissingContentException
-	 * @throws MissingAttributeValueException
-	 * @throws AlreadyExistsException
-	 * @throws InvalidContextException
-	 * @throws MissingMdRecordException
-	 * @throws ReferencedResourceNotFoundException
-	 * @throws AuthenticationException
-	 * @throws AuthorizationException
-	 * @throws ContextNotFoundException
-	 * @throws InvalidContentException
-	 * @throws OptimisticLockingException
-	 * @throws RelationPredicateNotFoundException
-	 * @throws FileNotFoundException
-	 * @throws MissingMethodParameterException
-	 * @throws NotPublishedException
-	 * @throws InvalidStatusException
-	 * @throws ReadonlyViolationException
-	 * @throws InvalidXmlException
-	 * @see de.escidoc.core.om.ContextHandler#update(String, String)
-	 */
-	public String update(final String contextId, final String contextXml)
-			throws RemoteException, SystemException, MissingLicenceException,
-			ReadonlyVersionException, LockingException,
-			ComponentNotFoundException, MissingContentException,
-			MissingAttributeValueException, AlreadyExistsException,
-			InvalidContextException, MissingMdRecordException,
-			ReferencedResourceNotFoundException, AuthenticationException,
-			AuthorizationException, ContextNotFoundException,
-			InvalidContentException, OptimisticLockingException,
-			RelationPredicateNotFoundException, FileNotFoundException,
-			MissingMethodParameterException, NotPublishedException,
-			InvalidStatusException, ReadonlyViolationException,
-			InvalidXmlException {
+    /**
+     * See Interface for functional description.
+     * 
+     * @param contextId
+     * @param contextXml
+     * @return
+     * @throws RemoteException
+     * @throws SystemException
+     * @throws MissingLicenceException
+     * @throws ReadonlyVersionException
+     * @throws LockingException
+     * @throws ComponentNotFoundException
+     * @throws MissingContentException
+     * @throws MissingAttributeValueException
+     * @throws AlreadyExistsException
+     * @throws InvalidContextException
+     * @throws MissingMdRecordException
+     * @throws ReferencedResourceNotFoundException
+     * @throws AuthenticationException
+     * @throws AuthorizationException
+     * @throws ContextNotFoundException
+     * @throws InvalidContentException
+     * @throws OptimisticLockingException
+     * @throws RelationPredicateNotFoundException
+     * @throws FileNotFoundException
+     * @throws MissingMethodParameterException
+     * @throws NotPublishedException
+     * @throws InvalidStatusException
+     * @throws ReadonlyViolationException
+     * @throws InvalidXmlException
+     * @see de.escidoc.core.om.ContextHandler#update(String, String)
+     */
+    public String update(final String contextId, final String contextXml)
+        throws RemoteException, SystemException, MissingLicenceException,
+        ReadonlyVersionException, LockingException, ComponentNotFoundException,
+        MissingContentException, MissingAttributeValueException,
+        AlreadyExistsException, InvalidContextException,
+        MissingMdRecordException, ReferencedResourceNotFoundException,
+        AuthenticationException, AuthorizationException,
+        ContextNotFoundException, InvalidContentException,
+        OptimisticLockingException, RelationPredicateNotFoundException,
+        FileNotFoundException, MissingMethodParameterException,
+        NotPublishedException, InvalidStatusException,
+        ReadonlyViolationException, InvalidXmlException {
 
-		return put(PATH_CONTEXT + "/" + contextId, contextXml);
-	}
+        return put(PATH_CONTEXT + "/" + contextId, contextXml);
+    }
 
-	public String retrieve(final String contextId) throws RemoteException,
-			SystemException, MissingMethodParameterException,
-			AuthenticationException, ContextNotFoundException,
-			AuthorizationException {
+    public String retrieve(final String contextId) throws RemoteException,
+        SystemException, MissingMethodParameterException,
+        AuthenticationException, ContextNotFoundException,
+        AuthorizationException {
 
-		return get(PATH_CONTEXT + "/" + contextId);
-	}
+        return get(PATH_CONTEXT + "/" + contextId);
+    }
 
-	public String retrieveProperties(final String contextId)
-			throws RemoteException, SystemException,
-			MissingMethodParameterException, AuthenticationException,
-			ContextNotFoundException, AuthorizationException {
+    public String retrieveProperties(final String contextId)
+        throws RemoteException, SystemException,
+        MissingMethodParameterException, AuthenticationException,
+        ContextNotFoundException, AuthorizationException {
 
-		return get(PATH_CONTEXT + "/" + contextId + "/properties");
-	}
+        return get(PATH_CONTEXT + "/" + contextId + "/properties");
+    }
 
-	public String retrieveContexts(final String filter) throws RemoteException,
-			SystemException, MissingMethodParameterException,
-			AuthenticationException, AuthorizationException,
-			InvalidXmlException {
+    public String retrieveContexts(final String filter) throws RemoteException,
+        SystemException, MissingMethodParameterException,
+        AuthenticationException, AuthorizationException, InvalidXmlException {
 
-		return post(PATH_CONTEXT + "s/filter", filter);
-	}
+        return post(PATH_CONTEXT + "s/filter", filter);
+    }
 }
