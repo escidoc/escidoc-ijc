a8d83fe17b3b9759663313e5fc93321b0def2a3e
SWA <SWA@5c398c58-3d0a-0410-a38c-d0825f6816e0>
2010-04-30 10:36:30 +0000

    code format
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/branches/eSciDocCoreClient/1.2/eSciDocCoreClient/@35047 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- src/java/de/escidoc/core/resources/sb/wrapper/explain/MyRecordExplainType.java
+++ src/java/de/escidoc/core/resources/sb/wrapper/explain/MyRecordExplainType.java
@@ -1,45 +1,47 @@
 package de.escidoc.core.resources.sb.wrapper.explain;
 
 import gov.loc.www.zing.srw.RecordType;
-import de.escidoc.core.resources.sb.wrapper.search.MyStringFragmentSearch;
 
 public class MyRecordExplainType {
-private RecordType record;
 
-private MyStringFragmentExplain stringFragment;
+    private RecordType record;
 
+    private MyStringFragmentExplain stringFragment;
 
-public MyRecordExplainType(RecordType record) {
-    this.record = record;
-}
+    public MyRecordExplainType(final RecordType record) {
+        this.record = record;
+    }
 
-public RecordType getRecord() {
-    return record;
-}
+    public RecordType getRecord() {
+        return record;
+    }
 
-public java.lang.String getRecordSchema() {
-    return record.getRecordSchema();
-}
+    public java.lang.String getRecordSchema() {
+        return record.getRecordSchema();
+    }
 
-public java.lang.String getRecordPacking() {
-    return record.getRecordPacking();
-}
-public MyStringFragmentExplain getStringFragment() {
-    return new MyStringFragmentExplain(record.getRecordData());
-}
+    public java.lang.String getRecordPacking() {
+        return record.getRecordPacking();
+    }
 
-public void setStringFragment(MyStringFragmentExplain stringFragment) {
-    this.stringFragment = stringFragment;
-}
+    public MyStringFragmentExplain getStringFragment() {
+        return new MyStringFragmentExplain(record.getRecordData());
+    }
 
+    public MyStringFragmentExplain setStringFragment() {
+        return this.stringFragment;
+    }
 
+    public void setStringFragment(final MyStringFragmentExplain stringFragment) {
+        this.stringFragment = stringFragment;
+    }
 
-public org.apache.axis.types.PositiveInteger getRecordPosition() {
-    return record.getRecordPosition();
-}
+    public org.apache.axis.types.PositiveInteger getRecordPosition() {
+        return record.getRecordPosition();
+    }
 
-public gov.loc.www.zing.srw.ExtraDataType getExtraRecordData() {
-    return record.getExtraRecordData();
-}
+    public gov.loc.www.zing.srw.ExtraDataType getExtraRecordData() {
+        return record.getExtraRecordData();
+    }
 
 }
--- src/java/de/escidoc/core/resources/sb/wrapper/explain/MyStringFragmentExplain.java
+++ src/java/de/escidoc/core/resources/sb/wrapper/explain/MyStringFragmentExplain.java
@@ -5,9 +5,12 @@ import de.escidoc.core.common.jibx.Marshaller;
 import de.escidoc.core.resources.sb.explain.ExplainRecord;
 
 public class MyStringFragmentExplain {
+
     StringOrXmlFragment stringOrXml;
-    private static final  Marshaller<ExplainRecord> m =
+
+    private static final Marshaller<ExplainRecord> m =
         new Marshaller<ExplainRecord>((new ExplainRecord()).getClass());
+
     private ExplainRecord resultRecord;
 
     public MyStringFragmentExplain(StringOrXmlFragment stringOrXml) {
@@ -17,31 +20,32 @@ public class MyStringFragmentExplain {
     public ExplainRecord getResultRecord() throws Exception {
         org.apache.axis.message.MessageElement[] messages =
             this.stringOrXml.get_any();
-            String recordData = decodeCharacters(messages[0].getAsString());
-            String resource = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" 
-                                           + recordData;
-            this.resultRecord = m.unmarshalDocument(resource);
-            return this.resultRecord;
+        String recordData = decodeCharacters(messages[0].getAsString());
+        String resource =
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + recordData;
+        this.resultRecord = m.unmarshalDocument(resource);
+        return this.resultRecord;
     }
 
     public void setResultRecord(ExplainRecord resultRecord) {
         this.resultRecord = resultRecord;
     }
+
     /**
      * Replaces special Characters..
      * 
      * @return String Replaced String
      * @param text
      *            String text to replace
-     * 
-     * @sb
      */
-    private String decodeCharacters(String text) {
-        text = text.replaceAll("&lt;", "<");
-        text = text.replaceAll("&gt;", ">");
-        text = text.replaceAll("&quot;", "\"");
-        text = text.replaceAll("&apos;", "'");
+    private String decodeCharacters(final String text) {
+
+        String tmp = text;
+        tmp = tmp.replaceAll("&lt;", "<");
+        tmp = tmp.replaceAll("&gt;", ">");
+        tmp = tmp.replaceAll("&quot;", "\"");
+        tmp = tmp.replaceAll("&apos;", "'");
         // text = text.replaceAll("&amp;", "&");
-        return text;
+        return tmp;
     }
 }
