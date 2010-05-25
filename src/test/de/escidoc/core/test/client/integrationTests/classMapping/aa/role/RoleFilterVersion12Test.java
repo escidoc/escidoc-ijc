86bc6544d4d707b8c8854a4a8f6140e21c0b9bfc
SWA <SWA@5c398c58-3d0a-0410-a38c-d0825f6816e0>
2010-05-25 15:30:35 +0000

    add test for version 1.2 filter for Roles
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/branches/eSciDocCoreClient/1.2/eSciDocCoreClient/@35516 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- src/test/de/escidoc/core/test/client/integrationTests/classMapping/aa/role/RoleFilterVersion12Test.java
+++ src/test/de/escidoc/core/test/client/integrationTests/classMapping/aa/role/RoleFilterVersion12Test.java
@@ -21,7 +21,7 @@
  */
 
 /*
- * Copyright 2006-2010 Fachinformationszentrum Karlsruhe Gesellschaft
+ * Copyright 2006-2008 Fachinformationszentrum Karlsruhe Gesellschaft
  * fuer wissenschaftlich-technische Information mbH and Max-Planck-
  * Gesellschaft zur Foerderung der Wissenschaft e.V.  
  * All rights reserved.  Use is subject to license terms.
@@ -29,7 +29,8 @@
 package de.escidoc.core.test.client.integrationTests.classMapping.aa.role;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
+import gov.loc.www.zing.srw.SearchRetrieveRequestType;
 
 import java.io.IOException;
 import java.util.Collection;
@@ -39,6 +40,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.axis.types.NonNegativeInteger;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -46,38 +48,38 @@ import org.xml.sax.SAXException;
 
 import de.escidoc.core.client.Authentication;
 import de.escidoc.core.client.RoleHandlerClient;
-import de.escidoc.core.client.exceptions.EscidocClientException;
-import de.escidoc.core.client.exceptions.EscidocException;
+import de.escidoc.core.client.UserAccountHandlerClient;
 import de.escidoc.core.client.exceptions.InternalClientException;
-import de.escidoc.core.client.exceptions.application.notfound.RoleNotFoundException;
 import de.escidoc.core.common.jibx.Factory;
 import de.escidoc.core.common.jibx.Marshaller;
 import de.escidoc.core.resources.aa.role.Role;
 import de.escidoc.core.resources.aa.role.RoleProperties;
-import de.escidoc.core.resources.aa.role.Roles;
 import de.escidoc.core.resources.aa.role.Scope;
 import de.escidoc.core.resources.aa.role.ScopeDef;
-import de.escidoc.core.resources.common.Filter;
-import de.escidoc.core.resources.common.TaskParam;
+import de.escidoc.core.resources.aa.useraccount.UserAccount;
+import de.escidoc.core.resources.sb.srw.SearchRetrieveResponseType;
 import de.escidoc.core.test.client.Constants;
 import de.escidoc.core.test.client.EscidocClientTestBase;
 import de.escidoc.core.test.client.util.Template;
 
 /**
- * Test client lib role handler.
+ * Test the CQL Container Filter.
  * 
+ * @author SWA
  * 
  */
-public class RoleHandlerClientTest {
+public class RoleFilterVersion12Test {
+
+    public static final String FILTER_PARAMETER_QUERY = "query";
 
     /**
-     * Test to create and retrieve user account.
+     * Test retrieving Roles through filter request (filter for version 1.2).
      * 
      * @throws Exception
      *             Thrown if anythings failed.
      */
     @Test
-    public void testCreateAndRetrieveSuccessfulRole() throws Exception {
+    public void testRetrieveRoles01() throws Exception {
 
         Authentication auth =
             new Authentication(EscidocClientTestBase.DEFAULT_SERVICE_URL,
@@ -90,122 +92,28 @@ public class RoleHandlerClientTest {
         Role role = createRole();
         Role createdRole = rc.create(role);
 
-        String objId = createdRole.getObjid();
-
-        Factory.getRoleMarshaller().marshalDocument(rc.retrieve(objId));
-
-    }
-
-    /**
-     * 
-     * @throws Exception
-     */
-    @Test
-    public void testUpdateSuccessfulRole() throws Exception {
-
-        Authentication auth =
-            new Authentication(EscidocClientTestBase.DEFAULT_SERVICE_URL,
-                Constants.SYSTEM_ADMIN_USER, Constants.SYSTEM_ADMIN_PASSWORD);
-
-        RoleHandlerClient rc = new RoleHandlerClient();
-        rc.setServiceAddress(EscidocClientTestBase.DEFAULT_SERVICE_URL);
-        rc.setHandle(auth.getHandle());
-
-        Role role = createRole();
-        Role createdRole = rc.create(role);
-        String newName = "newName" + System.currentTimeMillis();
-        createdRole.getProperties().setName(newName);
-        Role updatedRole = rc.update(createdRole);
-        String updatedName = updatedRole.getProperties().getName();
-        assertEquals(updatedName, newName);
-
-    }
-
-    /**
-     * 
-     * @throws Exception
-     */
-    @Test
-    public void testDeleteSuccessfulRole() throws Exception {
-
-        Role role = createRole();
         
-        // login
-        Authentication auth =
-            new Authentication(EscidocClientTestBase.DEFAULT_SERVICE_URL,
-                Constants.SYSTEM_ADMIN_USER, Constants.SYSTEM_ADMIN_PASSWORD);
+        // now check if at least this Container is in the list
 
-        RoleHandlerClient rc = new RoleHandlerClient();
-        rc.setServiceAddress(EscidocClientTestBase.DEFAULT_SERVICE_URL);
-        rc.setHandle(auth.getHandle());
-   
-        Role createdRole = rc.create(role);
+        UserAccountHandlerClient uac = new UserAccountHandlerClient();
+        uac.setServiceAddress(EscidocClientTestBase.DEFAULT_SERVICE_URL);
+        uac.setHandle(auth.getHandle());
+        UserAccount me = uac.retrieveCurrentUser();
 
-        String objId = createdRole.getObjid();
-        rc.delete(objId);
-        try {
-            rc.retrieve(objId);
-            fail("Missing Exception");
-        }
-        catch (EscidocException e) {
-            if (!(e instanceof RoleNotFoundException)) {
+        SearchRetrieveRequestType srwFilter = new SearchRetrieveRequestType();
+        srwFilter.setQuery("\"http://escidoc.de/core/01/structural-relations/created-by\"=" + me.getObjid());
+        srwFilter.setMaximumRecords(new NonNegativeInteger("1"));
 
-                fail("Wrong exception.Excepted exception of type "
-                    + "RoleNotFoundException but was " + e.getClass());
-            }
-        }
+        SearchRetrieveResponseType roleList = rc.retrieveRoles(srwFilter);
 
+        assertEquals("Wrong version number", "1.1", roleList.getVersion());
+        assertTrue("Wrong number of records",
+            roleList.getNumberOfRecords() >= 1);
+        assertEquals("Wrong record position", 1, roleList
+            .getRecords().iterator().next().getRecordPosition());
     }
 
-    /**
-     * Test serialize Role Filter task param object.
-     * 
-     * @throws InternalClientException
-     *             If internal client errors occur
-     */
-    @Test
-    public void testSerializeRole() throws InternalClientException {
-
-        TaskParam filterParam = new TaskParam();
-        Collection<Filter> filters = TaskParam.filtersFactory();
-
-        filters.add(getFilter("limited", "false", null));
-        filterParam.setFilters(filters);
-
-        // serialize data
-        Factory.getTaskParamMarshaller().marshalDocument(filterParam);
-    }
-
-    /**
-     * Test retrieving RetrieveUserAccounts through filter request.
-     * 
-     * @throws EscidocClientException
-     *             If errors occur on client internal, transport of framework
-     *             level
-     */
-    @Test
-    public void testRetrieveRoles() throws EscidocClientException {
-
-        TaskParam filterParam = new TaskParam();
-        Collection<Filter> filters = TaskParam.filtersFactory();
-
-        filters.add(getFilter("limited", "false", null));
-        filterParam.setFilters(filters);
-
-        Authentication auth =
-            new Authentication(EscidocClientTestBase.DEFAULT_SERVICE_URL,
-                Constants.SYSTEM_ADMIN_USER, Constants.SYSTEM_ADMIN_PASSWORD);
-
-        RoleHandlerClient rc = new RoleHandlerClient();
-        rc.setServiceAddress(EscidocClientTestBase.DEFAULT_SERVICE_URL);
-        rc.setHandle(auth.getHandle());
-
-        Roles roleList = rc.retrieveRoles(filterParam);
-
-        // test deserialize Role XML
-        Factory.getRoleListMarshaller().marshalDocument(roleList);
-    }
-
+    
     /**
      * Create a Role VO (but not create it at infrastructure).
      * 
@@ -269,27 +177,5 @@ public class RoleHandlerClientTest {
         Factory.getRoleMarshaller().marshalDocument(urole);
 
         return role;
-    }
-
-    /**
-     * Prepare and Filter class from the parameter collection.
-     * 
-     * @param name
-     *            TODO
-     * @param value
-     *            TODO
-     * @param ids
-     *            Collection of IDs
-     * @return Filter VO
-     */
-    private Filter getFilter(
-        final String name, final String value, final Collection<String> ids) {
-
-        Filter filter = new Filter();
-        filter.setName(name);
-        filter.setValue(value);
-        filter.setIds(ids);
-
-        return filter;
     }
 }
