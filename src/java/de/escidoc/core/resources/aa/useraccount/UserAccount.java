ee8242e3d2c4b5709337495e96c11a7334bce97e
chh <chh@5c398c58-3d0a-0410-a38c-d0825f6816e0>
2010-07-19 08:01:52 +0000

    add equals() and hashcode(). Use only User Accont's properties.
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/branches/eSciDocCoreClient/1.2/eSciDocCoreClient/@36414 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- src/java/de/escidoc/core/resources/aa/useraccount/UserAccount.java
+++ src/java/de/escidoc/core/resources/aa/useraccount/UserAccount.java
@@ -37,7 +37,7 @@ import de.escidoc.core.resources.om.GenericResource;
  * 
  */
 public class UserAccount extends GenericResource {
-    
+
     private UserAccountProperties properties;
 
     /**
@@ -57,6 +57,38 @@ public class UserAccount extends GenericResource {
      */
     public void setProperties(final UserAccountProperties properties) {
         this.properties = properties;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result =
+            prime * result + ((properties == null) ? 0 : properties.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!super.equals(obj)) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        UserAccount other = (UserAccount) obj;
+        if (properties == null) {
+            if (other.properties != null) {
+                return false;
+            }
+        }
+        else if (!properties.equals(other.properties)) {
+            return false;
+        }
+        return true;
     }
 
 }
