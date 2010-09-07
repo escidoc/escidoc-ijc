0115622c1abebec4618b38c9809d21a14531a641
chh <chh@5c398c58-3d0a-0410-a38c-d0825f6816e0>
2010-09-07 12:21:07 +0000

    Test case for retrieving Object ID for User Account.
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/branches/eSciDocCoreClient/1.2/eSciDocCoreClient-rest/eSciDocCoreClient/@37312 5c398c58-3d0a-0410-a38c-d0825f6816e0

---
+++ src/test/de/escidoc/core/test/client/integrationTests/RESTHandler/aa/user_account/ClientFactory.java
@@ -0,0 +1,23 @@
+package de.escidoc.core.test.client.integrationTests.RESTHandler.aa.user_account;
+
+import de.escidoc.core.client.Authentication;
+import de.escidoc.core.client.TransportProtocol;
+import de.escidoc.core.client.UserAccountHandlerClient;
+import de.escidoc.core.client.exceptions.EscidocClientException;
+import de.escidoc.core.test.client.Constants;
+import de.escidoc.core.test.client.EscidocClientTestBase;
+
+public class ClientFactory {
+
+    public static UserAccountHandlerClient createRestClientForUserAccount()
+        throws EscidocClientException {
+        final Authentication auth =
+            new Authentication(EscidocClientTestBase.DEFAULT_SERVICE_URL,
+                Constants.SYSTEM_ADMIN_USER, Constants.SYSTEM_ADMIN_PASSWORD);
+        final UserAccountHandlerClient client = new UserAccountHandlerClient();
+        client.setTransport(TransportProtocol.REST);
+        client.setServiceAddress(EscidocClientTestBase.DEFAULT_SERVICE_URL);
+        client.setHandle(auth.getHandle());
+        return client;
+    }
+}
---
+++ src/test/de/escidoc/core/test/client/integrationTests/RESTHandler/aa/user_account/RetrieveUserAccountsTest.java
@@ -0,0 +1,83 @@
+package de.escidoc.core.test.client.integrationTests.RESTHandler.aa.user_account;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.escidoc.core.client.UserAccountHandlerClient;
+import de.escidoc.core.client.exceptions.EscidocClientException;
+import de.escidoc.core.client.exceptions.EscidocException;
+import de.escidoc.core.client.exceptions.InternalClientException;
+import de.escidoc.core.client.exceptions.TransportException;
+import de.escidoc.core.resources.aa.useraccount.UserAccount;
+import de.escidoc.core.resources.aa.useraccount.UserAccounts;
+import de.escidoc.core.resources.common.Filter;
+import de.escidoc.core.resources.common.TaskParam;
+
+public class RetrieveUserAccountsTest {
+    private static final String SYSADMIN_OBJECT_ID = "escidoc:exuser1";
+
+    private UserAccountHandlerClient client;
+
+    @Before
+    public void setUp() throws EscidocClientException {
+        initUserAccountRestClient();
+        craeteTestUserAccount();
+    }
+
+    private void craeteTestUserAccount() throws EscidocException,
+        InternalClientException, TransportException {
+
+        final UserAccount fakeUserAccount =
+            FakeUserAccountFactory.createFakeUserAccount();
+
+        client.create(fakeUserAccount);
+    }
+
+    private void initUserAccountRestClient() throws EscidocClientException {
+        client = ClientFactory.createRestClientForUserAccount();
+    }
+
+    @Test
+    public void ShouldReturnAllUserAccounts() throws InternalClientException,
+        TransportException, EscidocClientException {
+        final UserAccounts usersCreatedBySysAdmin =
+            client.retrieveUserAccounts(createdBy(SYSADMIN_OBJECT_ID));
+
+        final Collection<UserAccount> userAccountsCreatedBySysAdmin =
+            usersCreatedBySysAdmin.getUserAccounts();
+
+        assertTrue("User Accounts created by SysAdmin should not be empty.",
+            !userAccountsCreatedBySysAdmin.isEmpty());
+
+        for (final UserAccount userAccount : userAccountsCreatedBySysAdmin) {
+            final String objid = userAccount.getObjid();
+            assertNotNull("Object Id should not be null. ", objid);
+        }
+    }
+
+    private static TaskParam createdBy(final String userAccountObjectId) {
+        final Collection<Filter> filters = TaskParam.filtersFactory();
+        filters.add(getFilter(
+            "http://escidoc.de/core/01/structural-relations/created-by",
+            userAccountObjectId, null));
+
+        final TaskParam filterParam = new TaskParam();
+        filterParam.setFilters(filters);
+        return filterParam;
+    }
+
+    private static Filter getFilter(
+        final String name, final String value, final Collection<String> ids) {
+
+        final Filter filter = new Filter();
+        filter.setName(name);
+        filter.setValue(value);
+        filter.setIds(ids);
+        return filter;
+    }
+}
