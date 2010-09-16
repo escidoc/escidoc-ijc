9b91a7accef2e67808f22eb63b5c264e9c20c542
chh <chh@5c398c58-3d0a-0410-a38c-d0825f6816e0>
2010-09-16 12:33:15 +0000

    Client does not thrown  de.escidoc.core.client.exceptions.application.security.AuthorizationException when the user does not have the right to execute the method.
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/branches/eSciDocCoreClient/1.2/eSciDocCoreClient-rest/eSciDocCoreClient/@37606 5c398c58-3d0a-0410-a38c-d0825f6816e0

---
+++ src/test/de/escidoc/core/test/client/integrationTests/classMapping/aa/login/SetHandlerTest.java
@@ -0,0 +1,79 @@
+package de.escidoc.core.test.client.integrationTests.classMapping.aa.login;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import de.escidoc.core.client.ContextHandlerClient;
+import de.escidoc.core.client.TransportProtocol;
+import de.escidoc.core.client.exceptions.EscidocClientException;
+import de.escidoc.core.client.exceptions.InternalClientException;
+import de.escidoc.core.client.exceptions.application.security.AuthorizationException;
+import de.escidoc.core.client.interfaces.ContextHandlerClientInterface;
+import de.escidoc.core.resources.ResourceRef;
+import de.escidoc.core.resources.ResourceRef.RESOURCE_TYPE;
+import de.escidoc.core.resources.om.context.AdminDescriptor;
+import de.escidoc.core.resources.om.context.AdminDescriptors;
+import de.escidoc.core.resources.om.context.Context;
+import de.escidoc.core.resources.om.context.OrganizationalUnitRefs;
+import de.escidoc.core.resources.om.context.Properties;
+import de.escidoc.core.test.client.EscidocClientTestBase;
+
+public class SetHandlerTest {
+
+    private ContextHandlerClientInterface cc;
+
+    @Before
+    public void setUp() throws InternalClientException {
+        cc = new ContextHandlerClient();
+        cc.setServiceAddress(EscidocClientTestBase.DEFAULT_SERVICE_URL);
+        cc.setTransport(TransportProtocol.REST);
+    }
+
+    @Test(expected = AuthorizationException.class)
+    public void shouldThrownAnExceptionIfHandleIsNotSet()
+        throws ParserConfigurationException, EscidocClientException {
+        createNewContext();
+    }
+
+    private Context createNewContext() throws ParserConfigurationException,
+        EscidocClientException {
+
+        final Context context = new Context();
+        final Properties properties = new Properties();
+        properties.setDescription("ContextDescription");
+        properties.setName("ContextName" + System.currentTimeMillis());
+        properties.setPublicStatus("opened");
+        properties.setPublicStatusComment("PublicStatusComment");
+
+        final OrganizationalUnitRefs organizationalUnitRefs =
+            new OrganizationalUnitRefs();
+        final ResourceRef organizationalUnitRef =
+            new ResourceRef("escidoc:ex3", RESOURCE_TYPE.OrganizationalUnit);
+        organizationalUnitRefs.add(organizationalUnitRef);
+        properties.setOrganizationalUnitRefs(organizationalUnitRefs);
+        properties.setType("type");
+        context.setProperties(properties);
+
+        final AdminDescriptors adminDescriptors = new AdminDescriptors();
+        final AdminDescriptor adminDescriptor = new AdminDescriptor();
+        adminDescriptor.setName("AdminDescriptorDemoName");
+        final DocumentBuilderFactory factory =
+            DocumentBuilderFactory.newInstance();
+        final DocumentBuilder builder = factory.newDocumentBuilder();
+        final Document doc = builder.newDocument();
+        final Element element = doc.createElementNS(null, "admin-descriptor");
+        adminDescriptor.setContent(element);
+
+        adminDescriptors.add(adminDescriptor);
+        context.setAdminDescriptors(adminDescriptors);
+
+        // create
+        return cc.create(context);
+    }
+}
