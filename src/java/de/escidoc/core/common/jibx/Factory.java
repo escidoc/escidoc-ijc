68b24e465e5b729c40f83be490b720a05b95fb4a
MVO <MVO@5c398c58-3d0a-0410-a38c-d0825f6816e0>
2010-09-17 11:40:09 +0000

    Fixed bug and cleanup.
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/branches/eSciDocCoreClient/1.2/eSciDocCoreClient-rest/eSciDocCoreClient/@37627 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- src/java/de/escidoc/core/common/jibx/Factory.java
+++ src/java/de/escidoc/core/common/jibx/Factory.java
@@ -8,49 +8,52 @@ import de.escidoc.core.client.exceptions.InternalClientException;
 import de.escidoc.core.common.configuration.ConfigurationProvider;
 
 public class Factory {
-	
-	private static final Map<TransportProtocol, MarshallerFactory> 
-		marshallerFactoryMap = 
-			new HashMap<TransportProtocol, MarshallerFactory>();
-	
-	private static final TransportProtocol defaultTransport = null;
-	
-	/**
-	 * 
-	 * @return
-	 * @throws InternalClientException
-	 */
-	public static final MarshallerFactory getMarshallerFactory()
-		throws InternalClientException {
-		
-		if(defaultTransport==null) {
-			TransportProtocol.valueOf(ConfigurationProvider.getInstance()
-					.getProperty(ConfigurationProvider.PROP_SERVICE_PROTOCOL));
-		}
-		if(marshallerFactoryMap.get(defaultTransport) == null) {
-			MarshallerFactory resultFactory = new MarshallerFactory(
-					defaultTransport);
-			marshallerFactoryMap.put(defaultTransport, resultFactory);
-			return resultFactory;
-		}
-		return marshallerFactoryMap.get(defaultTransport);
-	}
-	
-	/**
-	 * 
-	 * @param transport
-	 * @return
-	 * @throws InternalClientException
-	 */
-	public static final MarshallerFactory getMarshallerFactory(
-			TransportProtocol transport)
-		throws InternalClientException {
-	
-		if(marshallerFactoryMap.get(transport) == null) {
-			MarshallerFactory resultFactory = new MarshallerFactory(transport);
-			marshallerFactoryMap.put(transport, resultFactory);
-			return resultFactory;
-		}
-		return marshallerFactoryMap.get(transport);
-	}
+
+    private static final Map<TransportProtocol, MarshallerFactory> marshallerFactoryMap =
+        new HashMap<TransportProtocol, MarshallerFactory>();
+
+    private static TransportProtocol defaultTransport = null;
+
+    /**
+     * 
+     * @return
+     * @throws InternalClientException
+     */
+    public static final MarshallerFactory getMarshallerFactory()
+        throws InternalClientException {
+
+        if (defaultTransport == null) {
+            defaultTransport =
+                TransportProtocol.valueOf(ConfigurationProvider
+                    .getInstance().getProperty(
+                        ConfigurationProvider.PROP_SERVICE_PROTOCOL));
+        }
+        if (defaultTransport == null)
+            throw new InternalClientException(
+                "Unable to load default transport protocol from configuration.");
+        if (marshallerFactoryMap.get(defaultTransport) == null) {
+            MarshallerFactory resultFactory =
+                new MarshallerFactory(defaultTransport);
+            marshallerFactoryMap.put(defaultTransport, resultFactory);
+            return resultFactory;
+        }
+        return marshallerFactoryMap.get(defaultTransport);
+    }
+
+    /**
+     * 
+     * @param transport
+     * @return
+     * @throws InternalClientException
+     */
+    public static final MarshallerFactory getMarshallerFactory(
+        TransportProtocol transport) throws InternalClientException {
+
+        if (marshallerFactoryMap.get(transport) == null) {
+            MarshallerFactory resultFactory = new MarshallerFactory(transport);
+            marshallerFactoryMap.put(transport, resultFactory);
+            return resultFactory;
+        }
+        return marshallerFactoryMap.get(transport);
+    }
 }
