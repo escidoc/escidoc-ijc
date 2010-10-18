0c58fd20a3380673a64431bf13b9c690edc0ee50
MVO <MVO@5c398c58-3d0a-0410-a38c-d0825f6816e0>
2010-10-18 16:09:33 +0000

    - introduced new variable "keepInSync" for ADM requests - renamed filter-marshaller from "Filter" to "FilterMarshaller" - fixed bug with null values in Filter class, which caused the marshalling to fail
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/branches/eSciDocCoreClient/1.2/eSciDocCoreClient-rest/eSciDocCoreClient/@38551 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- src/main/java/de/escidoc/core/resources/common/TaskParam.java
+++ src/main/java/de/escidoc/core/resources/common/TaskParam.java
@@ -27,46 +27,9 @@ public class TaskParam {
 
     private Collection<Filter> filters = new LinkedList<Filter>();
 
-    private String[] ids;
-
-    /**
-     * TaskParam.
-     */
-    public TaskParam() {
-    }
-
-    /**
-     * TaskParam.
-     * 
-     * @param lastModificationDate
-     *            The last-modification-date.
-     * @param comment
-     *            The comment.
-     * @param pid
-     *            The Persistent ID.
-     * @param url
-     *            The URL.
-     * @param filters
-     *            Filters.
-     */
-    public TaskParam(final DateTime lastModificationDate, final String comment,
-        final String pid, final URL url, final Collection<Filter> filters) {
-
-        super();
-        this.lastModificationDate = lastModificationDate;
-        this.comment = comment;
-        this.pid = pid;
-        this.url = url;
-        this.filters = filters;
-    }
-
-    /**
-     * 
-     * @return Linked List with all filters.
-     */
-    public static LinkedList<Filter> filtersFactory() {
-        return new LinkedList<Filter>();
-    }
+    private Collection<String> ids = new LinkedList<String>();
+    
+    private boolean keepInSync = false;
 
     /**
      * @return the lastModificationDate
@@ -187,8 +150,7 @@ public class TaskParam {
      * 
      * @return resource references
      */
-    public String[] getResourceRefs() {
-
+    public Collection<String> getResourceRefs() {
         return this.ids;
     }
 
@@ -199,18 +161,20 @@ public class TaskParam {
      *            The objid of the new resoure reference
      */
     public void addResourceRef(final String objid) {
+        this.ids.add(objid);
+    }
 
-        if (this.ids == null) {
-            this.ids = new String[1];
-        }
-        else {
-            String[] t = new String[this.ids.length + 1];
-            for (int i = 0; i < this.ids.length; i++) {
-                t[i] = this.ids[i];
-            }
-            this.ids = t;
-        }
-        this.ids[this.ids.length - 1] = objid;
+    /**
+     * @param keepInSync the keepInSync to set
+     */
+    public void setKeepInSync(boolean keepInSync) {
+        this.keepInSync = keepInSync;
+    }
 
+    /**
+     * @return the keepInSync
+     */
+    public boolean isKeepInSync() {
+        return keepInSync;
     }
 }
