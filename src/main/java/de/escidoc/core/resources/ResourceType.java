12d0a32f0954d3fdd49a1753f2a684e3351ec119
MVO <MVO@5c398c58-3d0a-0410-a38c-d0825f6816e0>
2010-12-09 15:32:26 +0000

    Removed tag name definitions in enum.
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/branches/eSciDocCoreClient/1.2/eSciDocCoreClient-rest/eSciDocCoreClient/@39276 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- src/main/java/de/escidoc/core/resources/ResourceType.java
+++ src/main/java/de/escidoc/core/resources/ResourceType.java
@@ -6,42 +6,19 @@ package de.escidoc.core.resources;
  */
 public enum ResourceType {
     // root resources
-    Context(true, "context"), Item(true, "item"), Container(true,
-        "container"), OrganizationalUnit(true, "organizational-unit"), UserAccount(
-        true, "user-account"), ContentModel(true, "content-model"), Grant(
-        true, "grant"), Role(true, "role"), ContentRelation(true,
-        "content-relation"), Scope(true, "scope"), ReportDefinition(true,
-        "report-definition"), AggregationDefinition(true,
-        "aggregation-definition"),
+    Context(true), Item(true), Container(true), OrganizationalUnit(true), UserAccount(
+        true), ContentModel(true), Grant(true), Role(true), ContentRelation(
+        true), Scope(true), ReportDefinition(true), AggregationDefinition(true),
     // sub resources
-    Component(false, "component"), Toc(false, null), UserAccountAttribute(
-        false, null);
+    Component(false), Toc(false), UserAccountAttribute(false);
 
-    final boolean isRootResource;
+    private final boolean isRootResource;
 
-    final String tagName;
-
-    ResourceType(final boolean isRootResource, final String tagName) {
+    ResourceType(final boolean isRootResource) {
         this.isRootResource = isRootResource;
-        this.tagName = tagName;
     }
 
     public boolean isRootResource() {
         return isRootResource;
-    }
-
-    public String getTagName() {
-        return tagName;
-    }
-
-    public static final ResourceType valueByTagName(final String tagName) {
-        if (tagName == null)
-            return null;
-
-        for (int i = 0; i < values().length; i++) {
-            if (values()[i].tagName.equals(tagName))
-                return values()[i];
-        }
-        return null;
     }
 }
