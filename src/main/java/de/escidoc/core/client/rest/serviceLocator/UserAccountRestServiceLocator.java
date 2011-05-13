0f80a05b7443c45a124afc6081a00ff40be45f76
MVO <MVO@5c398c58-3d0a-0410-a38c-d0825f6816e0>
2011-05-13 12:56:02 +0000

    Added preconditions.

--- src/main/java/de/escidoc/core/client/rest/serviceLocator/UserAccountRestServiceLocator.java
+++ src/main/java/de/escidoc/core/client/rest/serviceLocator/UserAccountRestServiceLocator.java
@@ -1,5 +1,6 @@
 package de.escidoc.core.client.rest.serviceLocator;
 
+import static de.escidoc.core.common.Precondition.checkNotNull;
 import gov.loc.www.zing.srw.ExplainRequestType;
 import gov.loc.www.zing.srw.SearchRetrieveRequestType;
 
@@ -35,20 +36,36 @@ import de.escidoc.core.common.exceptions.remote.system.SystemException;
  * @author SWA
  * 
  */
+/**
+ * @author Marko Voß
+ * 
+ */
 public class UserAccountRestServiceLocator extends RestServiceMethod
     implements UserAccountHandler {
 
     public static final String PATH = "/aa/user-account";
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see de.escidoc.core.aa.UserAccountHandler#delete(java.lang.String)
+     */
     @Override
     public void delete(final String accountId) throws RemoteException,
         UserAccountNotFoundException, SystemException,
         MissingMethodParameterException, AuthenticationException,
         AuthorizationException {
 
+        checkNotNull(accountId);
+
         del(PATH + "/" + accountId);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see de.escidoc.core.aa.UserAccountHandler#create(java.lang.String)
+     */
     @Override
     public String create(final String userAccountXml) throws RemoteException,
         UniqueConstraintViolationException, SystemException,
@@ -56,9 +73,17 @@ public class UserAccountRestServiceLocator extends RestServiceMethod
         InvalidStatusException, AuthenticationException,
         AuthorizationException, InvalidXmlException {
 
+        checkNotNull(userAccountXml);
+
         return put(PATH, userAccountXml);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see de.escidoc.core.aa.UserAccountHandler#update(java.lang.String,
+     * java.lang.String)
+     */
     @Override
     public String update(final String accountId, final String userAccountXml)
         throws RemoteException, UserAccountNotFoundException,
@@ -71,15 +96,28 @@ public class UserAccountRestServiceLocator extends RestServiceMethod
         return put(PATH + "/" + accountId, userAccountXml);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see de.escidoc.core.aa.UserAccountHandler#retrieve(java.lang.String)
+     */
     @Override
     public String retrieve(final String accountId) throws RemoteException,
         UserAccountNotFoundException, SystemException,
         MissingMethodParameterException, AuthenticationException,
         AuthorizationException {
 
+        checkNotNull(accountId);
+
         return get(PATH + "/" + accountId);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see de.escidoc.core.aa.UserAccountHandler#activate(java.lang.String,
+     * java.lang.String)
+     */
     @Override
     public void activate(final String accountId, final String taskParam)
         throws RemoteException, UserAccountNotFoundException,
@@ -87,9 +125,17 @@ public class UserAccountRestServiceLocator extends RestServiceMethod
         MissingMethodParameterException, MissingAttributeValueException,
         AuthenticationException, AuthorizationException, InvalidXmlException {
 
+        checkNotNull(accountId);
+
         post(PATH + "/" + accountId + "/activate", taskParam);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see de.escidoc.core.aa.UserAccountHandler#deactivate(java.lang.String,
+     * java.lang.String)
+     */
     @Override
     public void deactivate(final String accountId, final String taskParam)
         throws RemoteException, UserAccountNotFoundException,
@@ -98,9 +144,18 @@ public class UserAccountRestServiceLocator extends RestServiceMethod
         AuthenticationException, AlreadyDeactiveException,
         AuthorizationException, InvalidXmlException {
 
+        checkNotNull(accountId);
+
         post(PATH + "/" + accountId + "/deactivate", taskParam);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.escidoc.core.aa.UserAccountHandler#updatePassword(java.lang.String,
+     * java.lang.String)
+     */
     @Override
     public void updatePassword(final String accountId, final String taskParam)
         throws RemoteException, UserAccountNotFoundException,
@@ -108,28 +163,55 @@ public class UserAccountRestServiceLocator extends RestServiceMethod
         MissingMethodParameterException, InvalidStatusException,
         AuthenticationException, AuthorizationException, InvalidXmlException {
 
+        checkNotNull(accountId);
+
         post(PATH + "/" + accountId + "/update-password", taskParam);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.escidoc.core.aa.UserAccountHandler#retrieveCurrentGrants(java.lang
+     * .String)
+     */
     @Override
     public String retrieveCurrentGrants(final String accountId)
         throws RemoteException, UserAccountNotFoundException, SystemException,
         MissingMethodParameterException, AuthenticationException,
         AuthorizationException {
 
+        checkNotNull(accountId);
+
         return get(PATH + "/" + accountId + "/resources/current-grants");
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.escidoc.core.aa.UserAccountHandler#retrieveGrant(java.lang.String,
+     * java.lang.String)
+     */
     @Override
     public String retrieveGrant(final String accountId, final String grantId)
         throws RemoteException, UserAccountNotFoundException, SystemException,
         MissingMethodParameterException, GrantNotFoundException,
         AuthenticationException, AuthorizationException {
 
+        checkNotNull(accountId);
+        checkNotNull(grantId);
+
         return get(PATH + "/" + accountId + "/resources/grants/grant/"
             + grantId);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see de.escidoc.core.aa.UserAccountHandler#createGrant(java.lang.String,
+     * java.lang.String)
+     */
     @Override
     public String createGrant(final String objid, final String userAccountXml)
         throws RemoteException, UserAccountNotFoundException, SystemException,
@@ -137,10 +219,18 @@ public class UserAccountRestServiceLocator extends RestServiceMethod
         AlreadyExistsException, AuthenticationException,
         AuthorizationException, InvalidXmlException {
 
+        checkNotNull(objid);
+
         return put(PATH + "/" + objid + "/resources/grants/grant",
             userAccountXml);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see de.escidoc.core.aa.UserAccountHandler#revokeGrant(java.lang.String,
+     * java.lang.String, java.lang.String)
+     */
     @Override
     public void revokeGrant(
         final String accountId, final String grantId, final String taskParam)
@@ -149,10 +239,19 @@ public class UserAccountRestServiceLocator extends RestServiceMethod
         GrantNotFoundException, AuthenticationException,
         AlreadyRevokedException, AuthorizationException, InvalidXmlException {
 
+        checkNotNull(accountId);
+        checkNotNull(grantId);
+
         post(PATH + "/" + accountId + "/resources/grants/grant/" + grantId
             + "/revoke-grant", taskParam);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see de.escidoc.core.aa.UserAccountHandler#revokeGrants(java.lang.String,
+     * java.lang.String)
+     */
     @Override
     public void revokeGrants(final String accountId, final String taskParam)
         throws RemoteException, UserAccountNotFoundException, SystemException,
@@ -160,37 +259,73 @@ public class UserAccountRestServiceLocator extends RestServiceMethod
         GrantNotFoundException, AuthenticationException,
         AlreadyRevokedException, AuthorizationException, InvalidXmlException {
 
+        checkNotNull(accountId);
+
         post(PATH + "/" + accountId + "/resources/grants/revoke-grants",
             taskParam);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.escidoc.core.client.interfaces.UserAccountHandler#retrieveGrants(gov
+     * .loc.www.zing.srw.SearchRetrieveRequestType)
+     */
     @Override
     public String retrieveGrants(final SearchRetrieveRequestType filter)
         throws RemoteException, SystemException,
         MissingMethodParameterException, AuthenticationException,
         AuthorizationException, InvalidXmlException {
 
+        checkNotNull(filter);
+
         return get("/aa/grants" + getEscidoc12Filter(filter));
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.escidoc.core.client.interfaces.UserAccountHandler#retrieveGrants(gov
+     * .loc.www.zing.srw.ExplainRequestType)
+     */
     @Override
     public String retrieveGrants(final ExplainRequestType filter)
         throws RemoteException, SystemException,
         MissingMethodParameterException, AuthenticationException,
         AuthorizationException, InvalidXmlException {
 
+        checkNotNull(filter);
+
         return get("/aa/grants" + getEscidoc12Filter(filter));
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.escidoc.core.aa.UserAccountHandler#retrievePreferences(java.lang.String
+     * )
+     */
     @Override
     public String retrievePreferences(final String accountId)
         throws RemoteException, UserAccountNotFoundException, SystemException,
         MissingMethodParameterException, AuthenticationException,
         AuthorizationException {
 
+        checkNotNull(accountId);
+
         return get(PATH + "/" + accountId + "/resources/preferences");
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.escidoc.core.aa.UserAccountHandler#createPreference(java.lang.String,
+     * java.lang.String)
+     */
     @Override
     public String createPreference(final String accountId, final String body)
         throws RemoteException, UserAccountNotFoundException,
@@ -198,10 +333,19 @@ public class UserAccountRestServiceLocator extends RestServiceMethod
         MissingMethodParameterException, AlreadyExistsException,
         AuthenticationException, AuthorizationException, InvalidXmlException {
 
+        checkNotNull(accountId);
+
         return put(
             PATH + "/" + accountId + "/resources/preferences/preference", body);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.escidoc.core.aa.UserAccountHandler#updatePreferences(java.lang.String,
+     * java.lang.String)
+     */
     @Override
     public String updatePreferences(final String accountId, final String body)
         throws RemoteException, UserAccountNotFoundException,
@@ -209,9 +353,18 @@ public class UserAccountRestServiceLocator extends RestServiceMethod
         MissingMethodParameterException, MissingAttributeValueException,
         AuthenticationException, AuthorizationException, InvalidXmlException {
 
+        checkNotNull(accountId);
+
         return put(PATH + "/" + accountId + "/resources/preferences", body);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.escidoc.core.aa.UserAccountHandler#updatePreference(java.lang.String,
+     * java.lang.String, java.lang.String)
+     */
     @Override
     public String updatePreference(
         final String accountId, final String prefName, final String body)
@@ -221,10 +374,19 @@ public class UserAccountRestServiceLocator extends RestServiceMethod
         MissingAttributeValueException, AlreadyExistsException,
         AuthenticationException, AuthorizationException, InvalidXmlException {
 
+        checkNotNull(accountId);
+
         return put(PATH + "/" + accountId
             + "/resources/preferences/preference/" + prefName, body);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.escidoc.core.aa.UserAccountHandler#retrievePreference(java.lang.String
+     * , java.lang.String)
+     */
     @Override
     public String retrievePreference(
         final String accountId, final String prefName) throws RemoteException,
@@ -232,10 +394,19 @@ public class UserAccountRestServiceLocator extends RestServiceMethod
         SystemException, MissingMethodParameterException,
         AuthenticationException, AuthorizationException {
 
+        checkNotNull(accountId);
+
         return get(PATH + "/" + accountId
             + "/resources/preferences/preference/" + prefName);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.escidoc.core.aa.UserAccountHandler#deletePreference(java.lang.String,
+     * java.lang.String)
+     */
     @Override
     public void deletePreference(final String accountId, final String prefName)
         throws RemoteException, UserAccountNotFoundException,
@@ -243,20 +414,38 @@ public class UserAccountRestServiceLocator extends RestServiceMethod
         MissingMethodParameterException, AuthenticationException,
         AuthorizationException {
 
+        checkNotNull(accountId);
+
         del(PATH + "/" + accountId + "/resources/preferences/preference/"
             + prefName);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.escidoc.core.aa.UserAccountHandler#createAttribute(java.lang.String,
+     * java.lang.String)
+     */
     @Override
     public String createAttribute(final String accountId, final String body)
         throws RemoteException, UserAccountNotFoundException, SystemException,
         MissingMethodParameterException, AlreadyExistsException,
         AuthenticationException, AuthorizationException, InvalidXmlException {
 
+        checkNotNull(accountId);
+
         return put(PATH + "/" + accountId + "/resources/attributes/attribute",
             body);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.escidoc.core.aa.UserAccountHandler#deleteAttribute(java.lang.String,
+     * java.lang.String)
+     */
     @Override
     public void deleteAttribute(final String accountId, final String attId)
         throws RemoteException, UserAccountNotFoundException,
@@ -264,9 +453,19 @@ public class UserAccountRestServiceLocator extends RestServiceMethod
         MissingMethodParameterException, ReadonlyElementViolationException,
         AuthenticationException, AuthorizationException {
 
+        checkNotNull(accountId);
+        checkNotNull(attId);
+
         del(PATH + "/" + accountId + "/resources/attributes/attribute/" + attId);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.escidoc.core.aa.UserAccountHandler#retrieveAttribute(java.lang.String,
+     * java.lang.String)
+     */
     @Override
     public String retrieveAttribute(final String accountId, final String attId)
         throws RemoteException, UserAccountNotFoundException,
@@ -274,19 +473,37 @@ public class UserAccountRestServiceLocator extends RestServiceMethod
         MissingMethodParameterException, AuthenticationException,
         AuthorizationException {
 
+        checkNotNull(accountId);
+        checkNotNull(attId);
+
         return get(PATH + "/" + accountId + "/resources/attributes/attribute/"
             + attId);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.escidoc.core.aa.UserAccountHandler#retrieveAttributes(java.lang.String
+     * )
+     */
     @Override
     public String retrieveAttributes(final String accountId)
         throws RemoteException, UserAccountNotFoundException, SystemException,
         MissingMethodParameterException, AuthenticationException,
         AuthorizationException {
 
+        checkNotNull(accountId);
+
         return get(PATH + "/" + accountId + "/resources/attributes");
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.escidoc.core.aa.UserAccountHandler#retrieveGrants(java.util.HashMap)
+     */
     @SuppressWarnings({ "rawtypes", "unchecked" })
     @Override
     @Deprecated
@@ -298,12 +515,22 @@ public class UserAccountRestServiceLocator extends RestServiceMethod
         return get("/aa/grants/filter", filter);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.escidoc.core.aa.UserAccountHandler#retrieveNamedAttributes(java.lang
+     * .String, java.lang.String)
+     */
     @Override
     public String retrieveNamedAttributes(
         final String accountId, final String attName) throws RemoteException,
         UserAccountNotFoundException, UserAttributeNotFoundException,
         SystemException, MissingMethodParameterException,
         AuthenticationException, AuthorizationException {
+
+        checkNotNull(accountId);
+        checkNotNull(attName);
 
         return get(PATH + "/" + accountId + "/resources/attributes/" + attName);
     }
@@ -339,18 +566,36 @@ public class UserAccountRestServiceLocator extends RestServiceMethod
         MissingMethodParameterException, AuthenticationException,
         AuthorizationException, InvalidXmlException {
 
+        checkNotNull(filter);
+
         return get(PATH + "s" + getEscidoc12Filter(filter));
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.escidoc.core.client.interfaces.UserAccountHandler#retrieveUserAccounts
+     * (gov.loc.www.zing.srw.ExplainRequestType)
+     */
     @Override
     public String retrieveUserAccounts(final ExplainRequestType filter)
         throws RemoteException, SystemException,
         MissingMethodParameterException, AuthenticationException,
         AuthorizationException, InvalidXmlException {
 
+        checkNotNull(filter);
+
         return get(PATH + "s" + getEscidoc12Filter(filter));
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.escidoc.core.aa.UserAccountHandler#updateAttribute(java.lang.String,
+     * java.lang.String, java.lang.String)
+     */
     @Override
     public String updateAttribute(
         final String accountId, final String attId, final String body)
@@ -360,10 +605,18 @@ public class UserAccountRestServiceLocator extends RestServiceMethod
         ReadonlyElementViolationException, AuthenticationException,
         AuthorizationException, InvalidXmlException {
 
+        checkNotNull(accountId);
+        checkNotNull(attId);
+
         return put(PATH + "/" + accountId + "/resources/attributes/attribute/"
             + attId, body);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see de.escidoc.core.aa.UserAccountHandler#retrieveCurrentUser()
+     */
     @Override
     public String retrieveCurrentUser() throws RemoteException,
         UserAccountNotFoundException, SystemException, AuthenticationException,
@@ -372,6 +625,13 @@ public class UserAccountRestServiceLocator extends RestServiceMethod
         return get(PATH + "/current");
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.escidoc.core.aa.UserAccountHandler#retrievePermissionFilterQuery(java
+     * .util.HashMap)
+     */
     @SuppressWarnings({ "rawtypes", "unchecked" })
     @Override
     public String retrievePermissionFilterQuery(final HashMap parameters)
